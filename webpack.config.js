e6f93d65fdc88010ce187cf1e411a4ca0a7eb8e3
Diana Shilova <Shilova_de@mail.ru>
2023-03-30 11:24:23 +0300

    fix error with Vercel (public dir)

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const config = {
     entry: './src/index.js',
     output: {
         filename: 'test.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'public'),
     },
     devServer: {
         open: true,
